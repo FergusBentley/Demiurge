8dfc7b9dd080359ecb41cd033798c5727b4657a3
Fergus Bentley <fergus.bentley@gmail.com>
2017-09-18 15:08:21 +0100

    Type may only be an extension of Tile

--- src/com/fergusbentley/asproj/world/VoronoiNode.java
+++ src/com/fergusbentley/asproj/world/VoronoiNode.java
@@ -3,9 +3,9 @@ package com.fergusbentley.asproj.world;
 public class VoronoiNode {
 
 	public int x, y;
-	public Class<?> type;
+	public Class<? extends Tile> type;
 	
-	public VoronoiNode(int x, int y, Class<?> t) {
+	public VoronoiNode(int x, int y, Class<? extends Tile> t) {
 		this.x = x;
 		this.y = y;
 		this.type = t;
